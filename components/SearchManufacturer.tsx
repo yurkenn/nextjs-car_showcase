998967ebe55320995345e57ed6551bae3fc89abb
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-03 20:22:06 +0300

    Create SearchManufacturer.tsx

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { SearchManufacturerProps } from '@/types';
+import { Combobox, Transition } from '@headlessui/react';
+
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full"></div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
