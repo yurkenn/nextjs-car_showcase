2a176a2d51e15c33d60d760c19f97a2f58a101ee
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-03 20:22:17 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import { footerLinks } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,12 +9,33 @@ const Footer = () => {
         <div className="flex flex-col justify-start items-start gap-6">
           <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
           <p className="text-base text-gray-700">
-            CarHub 2024 <br /> © All Rights Reserved
+            CarHub 2023 <br /> © All Rights Reserved
           </p>
         </div>
-        <div className="footer__links">
-          
 
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div className="footer__link" key={link.title}>
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link href={item.url} key={item.title} className="text-gray-500">
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub. All rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href={'/'} className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href={'/'} className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
