6e395b0ba51e6f40dd3541726bc3c22f96f751a8
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-05 22:38:50 +0300

    Create index.ts

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': 'e592d4f35bmshc01fa31c1570e09p1b8a71jsn94bb9b00e273',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    headers: headers,
+  });
+
+  const result = await response.json();
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
