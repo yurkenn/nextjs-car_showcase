60f8d04f4567deffb409cd55baa5583dc9fc0d7c
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-07 02:38:33 +0300

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,29 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  model: string;
+  fuel: string;
+  limit: number;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
